4eb062e9fc494d80521222d0f9b487e3568939dc
Karry <tomkarry@foxmail.com>
2025-05-29 23:25:04 +0800

    254 - Working on Sidebar

--- admin/src/components/Sidebar/index.js
+++ admin/src/components/Sidebar/index.js
@@ -1,7 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Sidebar () {
+
+    const menuItems = [
+        {text:"Dashboard", url:"/"},
+        {text:"Users", url:"/users"},
+        {text:"Product Type", url:"/product-type"},
+        {text:"Products", url:"/products"}
+    ]
+
     return (
         <div>
-            <h1>The Sidebar.</h1>
+            <div>
+                <h1>eStore</h1>
+            </div>
+            <ul>
+                {
+                    menuItems.map((menuItem, key) => {
+                        return (
+                            <li>
+                                <Link href={menuItem.url}>
+                                    <div>{menuItem.text}</div>
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
+            <div>
+                <div>
+                    <Image
+                        height={50}
+                        width={50}
+                        src="./user.svg"
+                        alt="User Avatar"
+                        radius="sm"
+                    />
+                    <div> John Doe </div>
+                </div>
+            </div>
         </div>
     )
 }
