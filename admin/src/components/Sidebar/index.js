ae29329e3763aa03f890edaa0be001c9a8ce2af8
Karry <tomkarry@foxmail.com>
2025-05-31 00:34:20 +0800

    257 - Styling the Sidebar

--- admin/src/components/Sidebar/index.js
+++ admin/src/components/Sidebar/index.js
@@ -11,17 +11,17 @@ export default function Sidebar () {
     ]
 
     return (
-        <div>
-            <div>
-                <h1>eStore</h1>
+        <div className="sidebar-main">
+            <div className="p-4 m-4">
+                <h1 className="text-3xl font-semibold">eStore</h1>
             </div>
-            <ul>
+            <ul className="mx-auto text-lg flex flex-col">
                 {
                     menuItems.map((menuItem, key) => {
                         return (
                             <li>
                                 <Link href={menuItem.url}>
-                                    <div>{menuItem.text}</div>
+                                    <div className="sidebar-list-item">{menuItem.text}</div>
                                 </Link>
                             </li>
                         )
@@ -29,16 +29,17 @@ export default function Sidebar () {
                 }
             </ul>
 
-            <div>
-                <div>
+            <div className="sidebar-usercard">
+                <div className="flex flex-row m-5 mb-8">
                     <Image
                         height={50}
                         width={50}
                         src="./user.svg"
                         alt="User Avatar"
                         radius="sm"
+                        className="border-gray-600 rounded-full border-2"
                     />
-                    <div> John Doe </div>
+                    <div className="m-auto text-lg"> John Doe </div>
                 </div>
             </div>
         </div>
