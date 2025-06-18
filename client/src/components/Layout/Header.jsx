0801041e8823bbf0880edc1855a1ad245559b539
Karry <tomkarry@foxmail.com>
2025-06-18 18:19:25 +0800

    309 - Adding Profile Dropdown

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,6 +1,13 @@
+'use client'
 import {CartIcon, SearchIcon, UserIcon} from "@/components/icons";
+import Link from "next/link";
+import {useState} from "react";
 
 const Header = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggleDropdown = ()=>{
+        setDropdownOpen(!dropdownOpen) ;
+    }
     return (
         <div className="navbar">
             <div className="container">
@@ -10,9 +17,25 @@ const Header = () => {
                         <SearchIcon className="absolute left-2 top-2 w-7 h-7"/>
                         <input placeholder="Search Product..." className="custom-input pl-10"/>
                     </div>
-                    <div className="flex gap-3">
-                        <CartIcon className="w-7 h-7"/>
-                        <UserIcon className="w-7 h-7"/>
+                    <div className="relative">
+                        <div className="flex gap-3">
+                            <CartIcon className="w-7 h-7"/>
+                            <button className="icon-button" onClick={toggleDropdown}>
+                                <UserIcon className="w-7 h-7"/>
+                            </button>
+                        </div>
+                        {
+                            dropdownOpen &&
+                            <div className="dropdown-menu">
+                                <Link href="/" className="block px-4 py-2 text-base text-gray-700 hover:bg-gray-100">My
+                                    Wish List</Link>
+                                <button
+                                    className="block px-4 py-2 text-base text-gray-700hover:bg-gray-100 w-full text-left">
+                                    Logout
+                                </button>
+                            </div>
+                        }
+
                     </div>
                 </div>
             </div>
