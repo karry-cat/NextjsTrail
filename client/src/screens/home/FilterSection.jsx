639610864838050313ac13444239b031cd1ffbce
Karry <tomkarry@foxmail.com>
2025-06-22 17:44:58 +0800

    319 - Continue on Filter Section

--- client/src/screens/home/FilterSection.jsx
+++ client/src/screens/home/FilterSection.jsx
@@ -5,11 +5,33 @@ import Accordion from "@/components/ui/Accordion";
 
 
 const FilterSection = ({searchParams}) => {
-    const categoryItems = [
+    const CategoryItems = [
         {label: "All", value: "all"},
         {label: "Kid's Clothing", value: "Kid's Clothing"},
         {label: "Men's Clothing", value: "Men's Clothing"}
     ]
+
+    const SortByItems = [
+        {label:"All",value:"all"},
+        {label:"Price high to low",value:"highToLow"},
+        {label:"Price low to high",value:"lowToHigh"}
+    ]
+
+    const RatingItems = [
+        {label:"All",value:"all"},
+        {label:"1", value:"1"},
+        {label:"2", value:"2"},
+        {label:"3",value:"3"},
+        {label:"4", value:"4"},
+        {label:"5", value:"5"}
+    ]
+
+    const AvailabilityItems = [
+        {label:"All",value:"all"},
+        {label:"In Stock",value:"true"},
+        {label:"Out of Stock", value:"false"}
+    ]
+
 
     const router = useRouter();
     const openAccordion = searchParams.openAccordion?.split(",") || [];
@@ -49,10 +71,86 @@ const FilterSection = ({searchParams}) => {
             >
                 <div className="flex flex-wrap gap-3 pt-2">
                     {
-                        categoryItems.map((item, index) => (
+                        CategoryItems.map((item, index) => (
                             <div key={index}>
                                 <input type="checkbox" id={`productType-${item.value}`} className="hidden peer"/>
                                 <label htmlFor={`productType-${item.value}`} className="checkbox-button-label">
+                                    {item.label}
+                                </label>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Accordion>
+            <Accordion
+                title="Sort By"
+                isOpened={openAccordion.includes("sortBy")}
+                type="sortBy"
+                handleAccordion={handleAccordion}
+            >
+                <div className="flex flex-wrap gap-3 pt-2">
+                    {
+                        SortByItems.map((item, index) => (
+                            <div key={index}>
+                                <input type="checkbox" id={`sortBy-${item.value}`} className="hidden peer"/>
+                                <label htmlFor={`sortBy-${item.value}`} className="checkbox-button-label">
+                                    {item.label}
+                                </label>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Accordion>
+            <Accordion
+                title="Price Range"
+                isOpened={openAccordion.includes("priceRange")}
+                type="priceRange"
+                handleAccordion={handleAccordion}
+            >
+                <div className="flex flex-wrap gap-3 pt-2">
+                    {
+                        CategoryItems.map((item, index) => (
+                            <div key={index}>
+                                <input type="checkbox" id={`productType-${item.value}`} className="hidden peer"/>
+                                <label htmlFor={`productType-${item.value}`} className="checkbox-button-label">
+                                    {item.label}
+                                </label>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Accordion>
+            <Accordion
+                title="Rating"
+                isOpened={openAccordion.includes("rating")}
+                type="rating"
+                handleAccordion={handleAccordion}
+            >
+                <div className="flex flex-wrap gap-3 pt-2">
+                    {
+                        RatingItems.map((item, index) => (
+                            <div key={index}>
+                                <input type="checkbox" id={`rating-${item.value}`} className="hidden peer"/>
+                                <label htmlFor={`rating-${item.value}`} className="checkbox-button-label">
+                                    {item.label}
+                                </label>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Accordion>
+            <Accordion
+                title="Availability"
+                isOpened={openAccordion.includes("inStock")}
+                type="inStock"
+                handleAccordion={handleAccordion}
+            >
+                <div className="flex flex-wrap gap-3 pt-2">
+                    {
+                        AvailabilityItems.map((item, index) => (
+                            <div key={index}>
+                                <input type="checkbox" id={`availability-${item.value}`} className="hidden peer"/>
+                                <label htmlFor={`availability-${item.value}`} className="checkbox-button-label">
                                     {item.label}
                                 </label>
                             </div>
