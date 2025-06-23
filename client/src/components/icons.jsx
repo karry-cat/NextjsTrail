3dcbcb952fb50d70464a88a2c3abf8c679245fa2
Karry <tomkarry@foxmail.com>
2025-06-24 00:10:08 +0800

    338 - Rendering Cart Item Card

--- client/src/components/icons.jsx
+++ client/src/components/icons.jsx
@@ -1,5 +1,7 @@
 import {cn} from "@/lib/util";
 
+//https://heroicons.com/
+
 export const SearchIcon = ({className}) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 strokeWidth={1.5} stroke="currentColor"
                                                 className={cn("size-6", className)}>
@@ -29,11 +31,22 @@ export const ChevronDownIcon = ({className}) => <svg xmlns="http://www.w3.org/20
 </svg>
 
 export const StarIcon = ({className}) => <svg xmlns="http://www.w3.org/2000/svg" fill="#f6b029" viewBox="0 0 24 24"
-                                              strokeWidth={1.5} stroke="currentColor" className={cn("size-6 text-[#f6b029]", className)}>
+                                              strokeWidth={1.5} stroke="currentColor"
+                                              className={cn("size-6 text-[#f6b029]", className)}>
     <path strokeLinecap="round" strokeLinejoin="round"
           d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"/>
 </svg>
 
+export const PlusCircleIcon = ({className}) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    strokeWidth={1.5} stroke="currentColor"
+                                                    className={cn("size-6", className)}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+</svg>
+
+export const MinusCircleIcon = ({className}) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                     strokeWidth={1.5} stroke="currentColor" className={cn("size-6", className)}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+</svg>
 
 
 
--- client/src/screens/cart/index.jsx
+++ client/src/screens/cart/index.jsx
@@ -1,6 +1,14 @@
+import Image from 'next/Image'
+import {MinusCircleIcon, PlusCircleIcon, StarIcon} from "@/components/icons";
+import {Button} from "@/components/ui/Button";
 
 export default function CartScreen({product}) {
 
+    const sizeOptions = [
+        {label: "s", value: "smallSize"},
+        {label: "M", value: "mediumSize"},
+        {label: "L", value: "largeSize"}
+    ]
 
     return (
         <div className="my-10">
@@ -9,7 +17,66 @@ export default function CartScreen({product}) {
             </h1>
             <div className="grid grid-cols-4 gap-5 my-5">
                 <div className="col-span-3 space-y-5">
-                    Cart Items
+                    <div className="w-full bg-white shadow-md rounded-xl">
+                        <div className="grid grid-cols-[auto_1fr]">
+                            <Image className="w-60 h-60 object-cover rounded-l-xl m-auto"
+                                   src={`/next.svg`}
+                                   alt="product"
+                                   width={0}
+                                   height={0}
+                                   sizes="100vw"/>
+                            <div className="flex flex-col p-8">
+                                <div className="flex justify-between">
+                                    <h1 className="text-2xl font-medium">Product Name</h1>
+                                    <div className="product-type-label">
+                                        Product Type
+                                    </div>
+                                </div>
+                                <div className="flex gap-x-1">
+                                    {
+                                        [...Array(5)].map((_, index) => (
+                                            <StarIcon key={index}/>
+                                        ))
+                                    }
+                                </div>
+                                <div className="text-xl flex gap-x-3 items-center">
+                                    <span className="text-gray-500 line-through font-medium">
+                                        $14.99
+                                    </span>
+                                    <span className="text-2xl font-semibold">
+                                        $12.99
+                                    </span>
+                                </div>
+                                <div className="flex gap-x-4 items-center">
+                                    <Button className="p-0 bg-transparent text-black">
+                                        <MinusCircleIcon className="w-8 h-8"/>
+                                    </Button>
+                                    <span className="text-xl font-semibold">1</span>
+                                    <Button className="p-0 bg-transparent text-black">
+                                        <PlusCircleIcon className="w-8 h-8"/>
+                                    </Button>
+                                </div>
+                                <div className="flex items-center gap-x-4">
+                                    <h6 className="text-lg font-semibold">Size</h6>
+                                    <div className="flex flex-wrap gap-3">
+                                        {
+                                            sizeOptions.map((size, index) => (
+                                                <div>
+                                                    <input type="radio"
+                                                           id={`sizes-${size.value}`}
+                                                           name="sizes"
+                                                           className="hidden peer"/>
+                                                    <label htmlFor="sizes" className="checkbox-button-label">
+                                                        {size.label}
+                                                    </label>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     Cart Summary
