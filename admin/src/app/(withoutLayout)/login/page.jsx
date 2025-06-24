a1ef1f0e2b328971bcc3c904e8143e0c2516831e
Karry <tomkarry@foxmail.com>
2025-06-24 14:41:27 +0800

    346 - Creating Login Page

---
+++ admin/src/app/(withoutLayout)/login/page.jsx
@@ -0,0 +1,10 @@
+import LoginScreen from "@/screens/login";
+
+const LoginPage = () => {
+    return (
+        <>
+            <LoginScreen/>
+        </>
+    )
+}
+export default LoginPage;
---
+++ admin/src/screens/login/index.js
@@ -0,0 +1,39 @@
+"use client"
+
+
+import Label from "@/components/ui/Label";
+import {Input} from "@/components/ui/Input";
+import {Button} from "@/components/ui/Button";
+
+const LoginScreen = () => {
+
+    return (
+        <div className="h-screen bg-gray-100 flex justify-center items-center">
+            <div className="w-full max-w-xl rounded-xl shadow-lg p-10 border border-gray-200 bg-white">
+                <h1 className="text-4xl font-medium text-center mb-7">Admin Login</h1>
+                <form className="grid gap-6">
+                    <div className="grid gap-2">
+                        <Label required>UserName</Label>
+                        <Input type="text"
+                               placeholder="Enter UserName"
+                               name="userName"
+                               required/>
+                    </div>
+                    <div className="grid gap-2">
+                        <Label required>Password</Label>
+                        <Input type="password"
+                               minlength={8}
+                               placeholder="Enter Your Password"
+                               name="password"
+                               required/>
+                    </div>
+                    <Button type="submit" className="w-full">
+                        Submit
+                    </Button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen;
