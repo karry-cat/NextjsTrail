afe657bbb7a96a9b4e5f216c6cfd5c0130813332
Karry <tomkarry@foxmail.com>
2025-06-24 16:57:07 +0800

    352 - Handling Private and Public routes using middleware

---
+++ admin/src/middleware.js
@@ -0,0 +1,17 @@
+import {NextResponse} from "next/server";
+import {verifyJWT} from "@/lib/util";
+
+export default async function handler(req, res) {
+    const token = req?.cookies?.get("jwt_token")?.value;
+    const publicRoutes = ["/login"];
+    const isValidToken = await verifyJWT(token);
+    if (!isValidToken && !publicRoutes.includes(req.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    } else if (isValidToken && publicRoutes.includes(req.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+}
+
+export const config = {
+    matcher: ["/((?!_next/static|_next/image|favicon\\.ico|api|uploads).*)"]
+}
