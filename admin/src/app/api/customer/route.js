36d049b86a46678fd7ebabf7387c40c3c638ebfc
Karry <tomkarry@foxmail.com>
2025-06-24 22:00:12 +0800

    365 - API to Fetch Unique Customers data

---
+++ admin/src/app/api/customer/route.js
@@ -0,0 +1,38 @@
+import {NextResponse} from "next/server";
+import {db} from "@/lib/db";
+import {verifyJWT} from "@/lib/util";
+
+export async function GET(request) {
+    try {
+        const token = request?.cookies?.get("customer_jwt_token")?.value;
+        const decodedToken = await verifyJWT(token);
+        if(!decodedToken){
+            return NextResponse.json(
+                {
+                    message: "Unauthorized.",
+                }, {
+                    status: 401
+                }
+            );
+        }
+        const customerData = await db.buyerMaster.findUnique({
+            where: {
+                email: decodedToken.email
+            }
+        })
+        return NextResponse.json(
+            {
+                status: 200,
+                message: "Custom's data fetched successfully!",
+                data: customerData
+            }
+        )
+    } catch (error) {
+        return NextResponse.json({
+            message: "Something went wrong.",
+            error: error.message
+        }, {
+            status: 500
+        })
+    }
+}
