e664a3c1e1dc78d411e6c87b560fcf08be998bea
Karry <tomkarry@foxmail.com>
2025-08-01 00:28:45 +0800

    391 - Preparing Customers Report using Chart

--- admin/src/actions/DashboardActions.js
+++ admin/src/actions/DashboardActions.js
@@ -1,4 +1,5 @@
 import {db} from "@/lib/db";
+import {formatDate} from "@/lib/util";
 
 export async function getDashboardData() {
     const customerData = await db.buyerMaster.findMany({
@@ -9,6 +10,20 @@ export async function getDashboardData() {
             sales: true
         }
     });
+
+    const customersByDate = customerData.reduce((acc, customer) => {
+        const date = formatDate(customer.createdAt);
+        if (!acc[date]) {
+            acc[date] = {
+                date,
+                count: 0,
+            };
+        }
+        acc[date].count += 1;
+        return acc;
+    }, {});
+    // console.log(Object.values(customersByDate));
+
     const salesMasterData = await db.salesMaster.findMany({
         take: 5,
         include: {
@@ -20,15 +35,15 @@ export async function getDashboardData() {
         }
     });
     //todo change it to postgre compatible
+    //sqlite version
     const salesByDate = await db.$queryRaw`
-        SELECT 
-            strftime('%m/%d/%Y', SODateTime / 1000, 'unixepoch') AS date,
+        SELECT strftime('%m/%d/%Y', SODateTime / 1000, 'unixepoch') AS date,
             SUM(grandTotalPrice) AS sales
         FROM SalesMaster
         GROUP BY date
         ORDER BY date desc;
     `;
-    // console.log(result);
+    // console.log(salesByDate);
     const totalRevenue = await db.salesMaster.aggregate({
         _sum: {
             grandTotalPrice: true
@@ -40,7 +55,8 @@ export async function getDashboardData() {
         totalCustomers: customerData.length,
         totalRevenue: totalRevenue._sum.grandTotalPrice,
         orders: salesMasterData,
-        salesChartData: salesByDate
+        salesChartData: salesByDate,
+        customerChartData:Object.values(customersByDate)
 
     }
     return dashboardData;
--- admin/src/screens/dashboard/DashboardScreen.jsx
+++ admin/src/screens/dashboard/DashboardScreen.jsx
@@ -20,10 +20,18 @@ export default function DashboardScreen({dashboardData}){
                 </div>
             </div>
             <RecentOrderSection orders={dashboardData.orders}/>
-            <div className="w-full dashboard-card">
-                <h1 className="text-2xl font-bold">Sales</h1>
-                <div className="w-full h-[300px] text-blue-700">
-                    <CustomLineChart data={dashboardData?.salesChartData} yKey="sales"/>
+            <div className="grid grid-cols-2 gap-2">
+                <div className="w-full dashboard-card">
+                    <h1 className="text-2xl font-bold">Sales</h1>
+                    <div className="w-full h-[300px] text-blue-700">
+                        <CustomLineChart data={dashboardData?.salesChartData} yKey="sales"/>
+                    </div>
+                </div>
+                <div className="w-full dashboard-card">
+                    <h1 className="text-2xl font-bold">Customers</h1>
+                    <div className="w-full h-[300px] text-green-400">
+                        <CustomLineChart data={dashboardData?.customerChartData} yKey="count"/>
+                    </div>
                 </div>
             </div>
         </div>
