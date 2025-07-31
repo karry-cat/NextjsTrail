45fd9ec64b58c769cdbd6022e9d42537ab8559e8
Karry <tomkarry@foxmail.com>
2025-08-01 00:33:20 +0800

    392 - Creating Chart Section Component

--- admin/src/screens/dashboard/DashboardScreen.jsx
+++ admin/src/screens/dashboard/DashboardScreen.jsx
@@ -1,5 +1,5 @@
 import RecentOrderSection from "@/screens/dashboard/RecentOrderSection";
-import CustomLineChart from "@/components/CustomLineChart";
+import ChartSection from "@/screens/dashboard/ChartSection";
 
 export default function DashboardScreen({dashboardData}){
     // console.log(dashboardData);
@@ -20,20 +20,7 @@ export default function DashboardScreen({dashboardData}){
                 </div>
             </div>
             <RecentOrderSection orders={dashboardData.orders}/>
-            <div className="grid grid-cols-2 gap-2">
-                <div className="w-full dashboard-card">
-                    <h1 className="text-2xl font-bold">Sales</h1>
-                    <div className="w-full h-[300px] text-blue-700">
-                        <CustomLineChart data={dashboardData?.salesChartData} yKey="sales"/>
-                    </div>
-                </div>
-                <div className="w-full dashboard-card">
-                    <h1 className="text-2xl font-bold">Customers</h1>
-                    <div className="w-full h-[300px] text-green-400">
-                        <CustomLineChart data={dashboardData?.customerChartData} yKey="count"/>
-                    </div>
-                </div>
-            </div>
+            <ChartSection dashboardData={dashboardData}/>
         </div>
     )
 }
